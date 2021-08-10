23f55b7f6682731e03c9ddbc9149b23f23dba4dd
Danilo Novakovic <dakenzi97@gmail.com>
2021-08-10 23:48:34 +0200

    Updated scripts to include typescript files, and also expanded eslint with typescript-lint

--- src/shared/lib/query/queryStringify.ts
+++ src/shared/lib/query/queryStringify.ts
@@ -2,11 +2,13 @@
  * Converts `{key1:value1, key2:value2}` to `"key1=value1&key2=value2"`
  * @param filters - query parameters
  */
-export default function queryStringify(filters: Record<string, unknown> = {}): string {
+export default function queryStringify(
+  filters: Record<string, unknown> = {}
+): string {
   let query = "";
-  for (let filterName in filters) {
+  for (const filterName in filters) {
     if (!filters[filterName]) continue;
-    let keyValPair = `${filterName}=${filters[filterName]}`;
+    const keyValPair = `${filterName}=${filters[filterName]}`;
     if (query) {
       query += "&";
     }
