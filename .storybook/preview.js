dfb0c72ae63d7449eb462e98637f1905cb8dcac7
Danilo Novakovic <dakenzi97@gmail.com>
2021-10-03 19:22:20 +0200

    Added muiTheme addon to storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,7 @@
+import { muiTheme } from "storybook-addon-material-ui";
+
+export const decorators = [muiTheme()];
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +10,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-}
+};
